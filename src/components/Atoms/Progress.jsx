115750851f77ba914b144a23e39f10cbda3c5f84
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-02-22 15:10:23 -0500

    fix(progress.jsx): porcent width

--- src/components/Atoms/Progress.jsx
+++ src/components/Atoms/Progress.jsx
@@ -7,7 +7,7 @@ const Progress = ({ color, className = '' }) => (
     <div className={`progress np-element progress--${color} ${className}`}>
         <span
             className="progress__bar"
-            style={{ width: randomInteger(20, 80) }}
+            style={{ width: `${randomInteger(20, 80)}%` }}
         ></span>
     </div>
 );
